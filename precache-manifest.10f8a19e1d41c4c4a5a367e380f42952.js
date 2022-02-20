c6788a2089a9ad5c6e6425f7e354e001401cd9ef
Rúben Filipe <rubenfilipe.net@gmail.com>
2022-02-20 02:56:12 -0300

    Updates

---
+++ precache-manifest.10f8a19e1d41c4c4a5a367e380f42952.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "808f149771723581553642e14fc3632b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb94feace1b1afb538aa",
+    "url": "/static/css/main.8d655eea.chunk.css"
+  },
+  {
+    "revision": "e61f923feef2661c72a5",
+    "url": "/static/js/2.5b50be47.chunk.js"
+  },
+  {
+    "revision": "52e759fc696eacbd6875",
+    "url": "/static/js/3.6012b189.chunk.js"
+  },
+  {
+    "revision": "bb94feace1b1afb538aa",
+    "url": "/static/js/main.34df8f62.chunk.js"
+  },
+  {
+    "revision": "3a46d98da4f0bff836c9",
+    "url": "/static/js/runtime~main.13ebfaf4.js"
+  },
+  {
+    "revision": "de23cfd10cf64864582b5f50a5cd79d2",
+    "url": "/static/media/cv.de23cfd1.svg"
+  },
+  {
+    "revision": "4356fafa38c8208670955bf86786c637",
+    "url": "/static/media/github.4356fafa.svg"
+  },
+  {
+    "revision": "6c86c7eed1f2482ed8161e523d588e63",
+    "url": "/static/media/lattes.6c86c7ee.svg"
+  },
+  {
+    "revision": "f610ecfdd657a5e52c3ab08be3023abb",
+    "url": "/static/media/linkedin.f610ecfd.svg"
+  },
+  {
+    "revision": "9e135a9c10f3e808216cde39024abdd6",
+    "url": "/static/media/perfil.9e135a9c.jpeg"
+  },
+  {
+    "revision": "1ea1062c3d6f22897a8ed0222ffd43b6",
+    "url": "/static/media/portfolio.1ea1062c.svg"
+  }
+]);
